8fa88a9c9f6bdf79ced89f247c4afd6fb818107e
shahakshit02 <shahakshit02@gmail.com>
2022-01-25 12:44:42 -0500

    Create Payment app in angular

---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from '../shared/payment-detail.model';
+import { PaymentDetailService } from '../shared/payment-detail.service';
+
+// type temp = { 
+//   paymentDetailId: number;
+//     cardOwnerName: string;
+//     cardNumber: string;
+//     expirationDate: string;
+//     securityCode: string; 
+// }
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styles: [
+  ]
+})
+
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService,
+    private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+
+  populateForm(selectedRecord:PaymentDetail){
+    this.service.formData = Object.assign({},selectedRecord);
+  }
+
+  onDelete(id:number){
+    if(confirm('Are you sure to delete this record?'))
+    {
+    this.service.deletePaymentDetail(id)
+    .subscribe(
+      res =>{
+        this.service.refreshList();
+        this.toastr.error("Deleted Successfully","Payment Detail Register");
+      },
+      err =>{console.log(err)}
+    )
+    }
+  }
+  
+}
---
+++ src/app/shared/payment-detail.model.ts
@@ -0,0 +1,13 @@
+export class PaymentDetail {
+    paymentDetailId: number = 0;
+    cardOwnerName: string = '';
+    cardNumber: string = '';
+    expirationDate: string ='';
+    securityCode: string = ''; 
+
+    // constructor(){
+    //     this.paymentDetailId = 0;
+    //     this.cardOwnerName = "";
+        
+    // }
+}
